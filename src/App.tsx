ee9c924ccb7a03a68d0af57ea04d4995ed915f98
lippiya <oliviatiffe@gmail.com>
2025-05-08 18:19:12 +0700

    add upload page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import UploadPage from "./pages/Upload";
 // import AuthLayout from "./layouts/AuthLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<UploadPage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
